20eec86d42bba0a0623f9d0597f34816e242a2b1
ravelinejunior <junior.raveline@gmail.com>
2020-08-07 11:02:30 -0300

    tela produto (em produçao)

--- lib/screens/product_screen/product_screen.dart
+++ lib/screens/product_screen/product_screen.dart
@@ -9,6 +9,9 @@ class ProductScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    //cor
+    final Color primaryColor = Theme.of(context).primaryColor;
+
     return Scaffold(
       appBar: AppBar(
         actions: <Widget>[
@@ -21,6 +24,7 @@ class ProductScreen extends StatelessWidget {
         title: Text(product.name),
         centerTitle: true,
       ),
+      backgroundColor: Colors.white,
       body: ListView(
         children: [
           //imagens carroussel
@@ -31,12 +35,56 @@ class ProductScreen extends StatelessWidget {
                 return NetworkImage(url);
               }).toList(),
               dotBgColor: Colors.transparent,
-              dotSize: 5,
-              dotHorizontalPadding: 5,
+              dotIncreasedColor: primaryColor,
+              dotColor: Colors.blueAccent,
+              dotSize: 6,
               boxFit: BoxFit.cover,
               autoplay: true,
               autoplayDuration: Duration(seconds: 5),
               borderRadius: true,
+            ),
+          ),
+          //textos widget
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                //nome produto widget
+                Text(
+                  product.name,
+                  style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.w600),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                  child: Text(
+                    "A partir de",
+                    style: TextStyle(fontSize: 14.0, color: Colors.grey[600]),
+                  ),
+                ),
+
+                //preço widget
+                Text(
+                  "R\$29,99",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: primaryColor,
+                      fontSize: 20.0),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 8.0, bottom: 4.0),
+                  child: Text(
+                    "Descrição",
+                    style:
+                        TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                  ),
+                ),
+                //preço widget
+                Text(
+                  product.description,
+                  style: TextStyle(fontStyle: FontStyle.italic, fontSize: 16.0),
+                ),
+              ],
             ),
           ),
         ],
