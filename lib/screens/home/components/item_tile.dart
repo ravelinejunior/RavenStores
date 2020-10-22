afd5fa04ec482206136681a3a26b7e5ca4c17457
ravelinejunior <junior.raveline@gmail.com>
2020-10-22 09:39:06 -0300

    Arredondamento da interface pra melhor uix

--- lib/screens/home/components/item_tile.dart
+++ lib/screens/home/components/item_tile.dart
@@ -15,136 +15,129 @@ class ItemTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final homeManager = context.watch<HomeManager>();
     final Color colorP = Theme.of(context).primaryColor;
-    return AspectRatio(
-      aspectRatio: 1,
-      child: GestureDetector(
-        onTap: () {
-          //verificar se produto está vinculado a imagem
-          if (item.product != null) {
-            //buscar produto correspondente ao id
-            final product =
-                context.read<ProductManager>().findProductbyId(item.product);
-            //verificar se produto foi encontrado
-            if (product != null) {
-              Navigator.of(context).pushNamed('/product', arguments: product);
+    return ClipRRect(
+      borderRadius: BorderRadius.horizontal(
+        right: Radius.circular(30),
+        left: Radius.circular(30),
+      ),
+      child: AspectRatio(
+        aspectRatio: 1,
+        child: GestureDetector(
+          onTap: () {
+            //verificar se produto está vinculado a imagem
+            if (item.product != null) {
+              //buscar produto correspondente ao id
+              final product =
+                  context.read<ProductManager>().findProductbyId(item.product);
+              //verificar se produto foi encontrado
+              if (product != null) {
+                Navigator.of(context).pushNamed('/product', arguments: product);
+              }
             }
-          }
-        },
+          },
 
-        //excluir ou vincular produto na tela de home
-        onLongPress: homeManager.editing
-            ? () {
-                showDialog(
-                  context: context,
-                  builder: (_) {
-                    //recuperar produto por id tocada
-                    final product = context
-                        .read<ProductManager>()
-                        .findProductbyId(item.product);
+          //excluir ou vincular produto na tela de home
+          onLongPress: homeManager.editing
+              ? () {
+                  showDialog(
+                    context: context,
+                    builder: (_) {
+                      //recuperar produto por id tocada
+                      final product = context
+                          .read<ProductManager>()
+                          .findProductbyId(item.product);
 
-                    return AlertDialog(
-                      title: const Text(
-                        'Editar produto',
-                        textAlign: TextAlign.center,
-                      ),
-                      content: product != null
-                          ? ListTile(
-                              leading: item.image is String
-                                  ? Card(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(30.0),
+                      return AlertDialog(
+                        title: const Text(
+                          'Editar produto',
+                          textAlign: TextAlign.center,
+                        ),
+                        content: product != null
+                            ? ListTile(
+                                leading: item.image is String
+                                    ? Card(
+                                        child: FadeInImage.assetNetwork(
+                                          placeholder:
+                                              'assets/shimmerloadingodd.gif',
+                                          image: item.image as String,
+                                          fit: BoxFit.fill,
+                                        ),
+                                      )
+                                    : Card(
+                                        child: Image.file(
+                                          item.image as File,
+                                          fit: BoxFit.cover,
+                                        ),
                                       ),
-                                      child: FadeInImage.assetNetwork(
-                                        placeholder:
-                                            'assets/shimmerloadingodd.gif',
-                                        image: item.image as String,
-                                        fit: BoxFit.fill,
-                                      ),
-                                    )
-                                  : Card(
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(30.0),
-                                      ),
-                                      child: Image.file(
-                                        item.image as File,
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
-                              title: Text(
-                                '${product.name}',
-                                overflow: TextOverflow.fade,
-                                style: TextStyle(fontWeight: FontWeight.w600),
-                              ),
-                              subtitle: Text(
-                                'R\$ ${product.basePrice.toStringAsFixed(2)}',
-                                overflow: TextOverflow.fade,
-                                style: TextStyle(
-                                    color: colorP, fontWeight: FontWeight.bold),
-                              ),
-                            )
-                          : null,
-                      contentPadding: const EdgeInsets.all(16),
-                      actions: [
-                        FlatButton.icon(
-                          onPressed: () {
-                            context.read<Section>().removeItem(item);
-                            Navigator.of(context).pop();
-                          },
-                          icon: Icon(
-                            Icons.remove_circle,
-                            color: Colors.red,
-                          ),
-                          label: Text(
-                            'Excluir',
-                            style: TextStyle(
+                                title: Text(
+                                  '${product.name}',
+                                  overflow: TextOverflow.fade,
+                                  style: TextStyle(fontWeight: FontWeight.w600),
+                                ),
+                                subtitle: Text(
+                                  'R\$ ${product.basePrice.toStringAsFixed(2)}',
+                                  overflow: TextOverflow.fade,
+                                  style: TextStyle(
+                                      color: colorP,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              )
+                            : null,
+                        contentPadding: const EdgeInsets.all(16),
+                        actions: [
+                          FlatButton.icon(
+                            onPressed: () {
+                              context.read<Section>().removeItem(item);
+                              Navigator.of(context).pop();
+                            },
+                            icon: Icon(
+                              Icons.remove_circle,
                               color: Colors.red,
                             ),
+                            label: Text(
+                              'Excluir',
+                              style: TextStyle(
+                                color: Colors.red,
+                              ),
+                            ),
                           ),
-                        ),
 
-                        //vincular ou desvincular produto
-                        FlatButton.icon(
-                          onPressed: () async {
-                            //se produto vinculado, desvincular
-                            if (product != null) {
-                              item.product = null;
-                            } else {
-                              //vincular produto
-                              final Product product =
-                                  await Navigator.of(context)
-                                      .pushNamed('/selectedProduct') as Product;
+                          //vincular ou desvincular produto
+                          FlatButton.icon(
+                            onPressed: () async {
+                              //se produto vinculado, desvincular
+                              if (product != null) {
+                                item.product = null;
+                              } else {
+                                //vincular produto
+                                final Product product =
+                                    await Navigator.of(context)
+                                            .pushNamed('/selectedProduct')
+                                        as Product;
 
-                              //colocar o produto no item
-                              item.product = product?.id;
-                            }
-                            Navigator.of(context).pop();
-                          },
-                          icon: Icon(
-                            product != null
-                                ? Icons.leak_remove
-                                : Icons.add_to_home_screen,
-                            color: colorP,
-                          ),
-                          label: Text(
-                            product != null ? 'Desvincular' : 'Vincular',
-                            style: TextStyle(color: colorP),
-                          ),
-                        )
-                      ],
-                    );
-                  },
-                );
-              }
-            : null,
+                                //colocar o produto no item
+                                item.product = product?.id;
+                              }
+                              Navigator.of(context).pop();
+                            },
+                            icon: Icon(
+                              product != null
+                                  ? Icons.leak_remove
+                                  : Icons.add_to_home_screen,
+                              color: colorP,
+                            ),
+                            label: Text(
+                              product != null ? 'Desvincular' : 'Vincular',
+                              style: TextStyle(color: colorP),
+                            ),
+                          )
+                        ],
+                      );
+                    },
+                  );
+                }
+              : null,
 
-        child: Card(
-          elevation: 5,
-          shadowColor: Colors.grey,
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(30.0),
-          ),
           child: item.image is String
               ? FadeInImage.assetNetwork(
                   placeholder: 'assets/girlshopping.gif',
